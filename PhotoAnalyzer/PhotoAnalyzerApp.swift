45dd07fcd1890d5fefbc4c0005835a65f1c5393b
Thomas Amaranto <36237630+Tommy86ITA@users.noreply.github.com>
2026-06-15 15:03:58 +0200

    Initial Commit

---
+++ PhotoAnalyzer/PhotoAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAnalyzerApp.swift
+//  PhotoAnalyzer
+//
+//  Created by Thomas Amaranto on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
